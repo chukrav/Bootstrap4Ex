beaedd351e931905a368e3fdd84a9670db7cef7b
lesha K <alexei.jobs@gmail.com>
2022-11-19 22:00:56 +0200

    image responsive

---
+++ Bootstrap4/conFusion/my_tests/listCopy.bat
@@ -0,0 +1,12 @@
+copy item01.html     item02.html
+copy item01.html     item03.html
+copy item01.html     item04.html
+copy item01.html     item05.html
+copy item01.html     item06.html
+copy item01.html     item07.html
+copy item01.html     item08.html
+copy item01.html     item10.html
+copy item01.html     item11.html
+copy item01.html     item12.html
+copy item01.html     item13.html
+
