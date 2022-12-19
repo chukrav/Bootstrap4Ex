edb233c994b280cedede24f320c4a23d634ead7b
alexei <alexei.jobs@gmail.com>
2022-12-19 18:10:42 +0200

    polish two first

--- Bootstrap4/conFusion/my_tests/test_site01/js/carousele.js
+++ Bootstrap4/conFusion/my_tests/test_site01/js/carousele.js
@@ -32,3 +32,37 @@ const imagePressed = e => {
 function toggleCarousel(){
   $("#myModal").modal('toggle'); //see here usage
 };
+
+function setBanners(doc, values) {
+  doc.getElementById("imf-1").setAttribute("src", values.banners[0].src);
+  doc.getElementById("left-ban").setAttribute("href", values.banners[0].href);
+  doc.getElementById("overlay-txt-left").innerHTML = values.banners[0].text; 
+  doc.getElementById("imf-2").setAttribute("src", values.banners[1].src);
+  doc.getElementById("right-ban").setAttribute("href", values.banners[1].href);
+  doc.getElementById("overlay-txt-right").innerHTML = values.banners[1].text;
+}
+
+function setImages(doc, values) {
+  // const imgs = doc.getElementsByTagName("img");
+  console.log("imgs count: " + imgs.length);
+  let i = 0;
+  let icorrect = true;
+  for (let img of imgs) {
+    console.log(img.id);
+    if (img.id.includes("imb")) {
+      img.addEventListener("click", imagePressed);
+      img.setAttribute("src", values.images[i]);
+      i = i + 1;
+    }
+
+    if (img.id.includes("imm")) {
+      if ((i > 0) & (icorrect === true)) {
+        i = 0;
+        icorrect = false;
+        console.log("------ i = 0 -------- ");
+      }
+      img.setAttribute("src", values.images[i]);
+      i = i + 1;
+    }
+  }
+}
